9e8bca53e9081010d99e11d1f03eb12eb79369df
David Guttman <david@davidguttman.com>
2013-11-24 16:02:27 -0800

    added plug-login example

---
+++ examples/plug-login.js
@@ -0,0 +1,15 @@
+var plugLogin = require('../plug-login')
+
+var creds = {
+    username: 'xxx'
+  , password: 'yyy'
+}
+
+plugLogin(creds, function(err, jar) {
+  // console.log('jar', jar);
+  var cookie = jar.cookies.filter(function(cookie) {
+    if (cookie.name === 'usr') return true
+  })[0]
+  var cookieVal = cookie.value
+  console.log('cookieVal', cookieVal);
+})
