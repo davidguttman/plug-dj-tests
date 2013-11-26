49d4a9cdaaba30fef96fdfe6693410fee11cfc2e
David Guttman <david@davidguttman.com>
2013-11-25 20:55:41 -0800

    cleaned up API

--- index.js
+++ index.js
@@ -1,34 +1,27 @@
-var http = require('http')
-var uuid = require('uuid')
 var request = require('request')
-var cheerio = require('cheerio')
 var WebSocket = require('ws')
+var util = require('util')
+var events = require('events')
+var uuid = require('uuid')
 
-var calc = require('./calculator')
-
-var PlugAPI = require('plugapi')
-
-var botName = 'MrVisuals'
-var auth = 'xxxxx'
-var room = 'chillout-mixer-ambient-triphop'
-// var room = 'the-chillout-mixer'
-
-var opts = {
-    url: 'https://sio2.plug.dj/socket.io/1/?t=' + Date.now()
-  , headers: {
-      Cookie: 'usr="'+auth+'"'
-  }
+var PlugBot = module.exports = function(auth) {
+  events.EventEmitter.call(this)
+  this.connect(auth)
+  return this
 }
 
-function joinRoom (ws, roomName) {
+util.inherits(PlugBot, events.EventEmitter)
+
+PlugBot.prototype.joinRoom = function(roomName) {
   var roomOpts = {
       name: 'join'
     , args: [roomName]
   }
-  ws.send('5::/room:'+JSON.stringify(roomOpts))
+  console.log('joining room', roomName);
+  this.ws.send('5::/room:'+JSON.stringify(roomOpts))
 }
 
-function speak (ws, message) {
+PlugBot.prototype.speak = function(message) {
   var cid = uuid.v4().replace(/-/g,'').substr(0, 13)
   var message = {
       name: 'chat'
@@ -37,84 +30,95 @@ function speak (ws, message) {
       , chatID: cid
     }]
   }
-  ws.send('5::/room:'+JSON.stringify(message))
+  this.ws.send('5::/room:'+JSON.stringify(message))
 }
 
-function onMessage (ws, message) {
+PlugBot.prototype.connect = function(auth, cb) {
+  var self = this
+
+  this.getSocketUrl(auth, function(err, socketUrl) {
+    if (err) return self.emit('error', err)
+
+    self.ws = new WebSocket(socketUrl)
+    self.setEvents()
+  })
+}
+
+PlugBot.prototype.setEvents = function() {
+  var self = this
+
+  self.ws.on('error', function(err) {
+    self.emit('error', err)
+  })
+  
+  self.ws.on('open', function() {
+    self.ws.send('1::/room') // not sure if this is required...
+    self.emit('connect')
+  })
+
+  self.ws.on('close', function() {
+    self.emit('disconnect')
+  })
+
+  self.ws.on('message', function(data, flags) {
+    self.emit('data', data)
+    var message = self.parseMessage(data)
+    self.onData(message)
+  })
+  
+}
+
+PlugBot.prototype.onMessage = function(message) {
   console.log('message', message);
-  if (message.from !== botName) {
-    var re = /mrvisuals:?\s*/
-    if (message.message.toLowerCase().match(re)) {
-      var content = message.message.toLowerCase().split(re)[1]
-      var result = calc(content)
-      if (result) {
-        speak(ws, result)  
-      }
-      
-    }
+}
+
+PlugBot.prototype.onData = function(message) {
+  switch (message.type) {
+    case 'heartbeat':
+      return this.onHeartbeat()
+    case 'message':
+      return this.onMessage(message)
+    default:
+      return
   }
 }
 
-request(opts, function(err, res, body) {
-  var sockId = body.split(':')[0]
-  var sockUrl = 'wss://sio2.plug.dj/socket.io/1/websocket/' + sockId
-  var ws = new WebSocket(sockUrl)
-  ws.on('open', function() {
-    ws.send('1::/room')
+PlugBot.prototype.parseMessage = function(data) {
+  var message
 
-    joinRoom(ws, room)
-    // speak(ws, 'hello, I am a bot')
-  })
-  ws.on('message', function(data, flags) {
-    console.log('data', data);
-    if (data == '2::') {
-      ws.send('2::')
+  if (data == '2::') {
+    message = {type: 'heartbeat'}
+    return message
+  }
+
+  if (data.match(/^5::\/room:/)) {
+    var mStr = data.split('5::/room:')[1]
+    message = JSON.parse(mStr).args[0]
+    return message
+  }
+
+  return {type: 'unknown'}
+    
+}
+
+PlugBot.prototype.getSocketUrl = function(auth, cb) {
+  var opts = {
+      url: 'https://sio2.plug.dj/socket.io/1/?t=' + Date.now()
+    , headers: {
+        Cookie: 'usr=' + auth
     }
-    if (data.match(/^5::\/room:/)) {
-      var mStr = data.split('5::/room:')[1]
-      var m = JSON.parse(mStr).args[0]
-      if (m.type === 'message') {
-        onMessage(ws, m)  
-      }
-      
-    }
+  }
+
+  request(opts, function(err, res, body) {
+    if (err) return cb(err)
+
+    var sockId = body.split(':')[0]
+    var sockUrl = 'wss://sio2.plug.dj/socket.io/1/websocket/' + sockId
+
+    cb(null, sockUrl)
   })
-})
+}
 
-// var bot = new PlugAPI(auth)
-// bot.connect(room)
-
-// bot.on('connected', function() {
-//   console.log('connected!');
-//   bot.client.on('data', function(data) {
-//     console.log('data', data);
-//   })
-// })
-
-// bot.on('chat', function(data) {
-//   console.log('chat', data);
-// })
-
-// bot.on('data', function(data) {
-//   console.log('data', data);
-// })
-
-//////////////////
-
-// var plugLogin = require('./plug-login')
-
-// var creds = {
-//     username: 'xxx'
-//   , password: 'xxx'
-// }
-
-// plugLogin(creds, function(err, jar, cookies) {
-//   console.log('jar', jar);
-//   var cookie = jar.cookies.filter(function(cookie) {
-//     if (cookie.name === 'usr') return true
-//   })[0]
-//   var cookieVal = cookie.value
-//   console.log('cookieVal', cookieVal);
-
-  
-// })
+PlugBot.prototype.onHeartbeat = function() {
+  this.ws.send('2::')
+}
